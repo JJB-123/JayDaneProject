1ec25afb784efcae546f80682f34c843ae927d6c
brownj42 <brownj42@BROWNJ42-L4.ent.core.medtronic.com>
2015-10-23 14:45:11 -0500

    Removed Gender creation for PlayerToken into a new class - ported over code from GameBoard class.

--- src/jaydaneproject/GameBoard.java
+++ src/jaydaneproject/GameBoard.java
@@ -12,7 +12,7 @@ public class GameBoard {
     String GLOBALSEED;
     String PLAYERNAME;
     int[] SEEDVALUES;
-    String[][] PLAYERTOKENPAIRS; // Key, Value ex: ["GENDER"]["FEMALE"]
+
     
     // Constructor - Needed to build object
     public GameBoard(String seed, String playerName, int[] letterCount){
@@ -20,23 +20,7 @@ public class GameBoard {
         this.PLAYERNAME = playerName;        
         this.SEEDVALUES = letterCount;
     }
-    
-    public void GeneratePlayerTokenSeed(int[] _SEEDVALUES){
-        
-        // count of Zeros in _SEEDVALUES
-        int zeroCount = 0;
-        // Loop through the number array - Pulling out non-zero values
-        for (int x = 0; x < _SEEDVALUES.length; x++){          
-            
-            // Use the 'count of zeros' to determine Gender
-            if(_SEEDVALUES[x] == 0){
-                zeroCount = zeroCount + 1;
-            }  
-        }
-         
-        // Debug line - Print out the # of 0's in the SEEDVALUES array
-        System.out.print(zeroCount);    
-    }
+   
     
     public static void Run(String[] args) {
         
